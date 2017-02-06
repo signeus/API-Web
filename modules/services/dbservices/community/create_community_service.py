4ebb046460ef28e81ec9f1bbc096637e5f4398e3
Kev <kev.ms93@gmail.com>
2017-02-06 19:15:36 +0100

    Images from New Community

--- modules/services/dbservices/community/create_community_service.py
+++ modules/services/dbservices/community/create_community_service.py
@@ -13,16 +13,23 @@ class CreateCommunityService (IService):
 		image = self.parameters.get("banner", None)
 		self.parameters.pop("banner", None)
 		#del self.parameters["banner"]
-		record =  DBService().insertIn2Collection("Communities", self.parameters)
-		result = self.core.UserOperation("suscribeUser2Community", {"id_user":record["id_creator"],"id_community":record["_id"]})
-		if image:
-			##Saving the image
-			path = "/home/kevin/Pictures/banners/" + str(record["_id"]) + ".png"
-			avatarFile = open(path, "wb")
-			decodeFile = base64.b64decode(image)
-			##encodeFile = base64.b64encode(self.parameters["avatar"])
-			avatarFile.write(decodeFile)
-			avatarFile.close()
-			##End Saving the image
+		try:
+			record =  DBService().insertIn2Collection("Communities", self.parameters)
+			result = self.core.UserOperation("suscribeUser2Community", {"id_user":record["id_creator"],"id_community":record["_id"]})
+			if image:
+				##Saving the image
+				print "He entrado en la image"
+				path = "/home/kevin/Pictures/banners/" + str(record["_id"]) + ".png"
+				avatarFile = open(path, "wb")
+				print str(record["_id"])
+				image = str(image)
+				format = image[:image.find(",")]
+				image = image[image.find(",") + 1:]
+				decodeFile = base64.b64decode(image)
+				avatarFile.write(decodeFile)
+				avatarFile.close()
+				##End Saving the image
+		except Exception, ex:
+			print ex
 		return record
 
