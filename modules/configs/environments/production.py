318c4149db1b5bffd8e38580f2d7f14be2fee475
Kev <kev.ms93@gmail.com>
2017-02-20 14:37:49 +0100

    New configuration Pro

---
+++ modules/configs/environments/production.py
@@ -0,0 +1,19 @@
+import socket
+from configs.i_enviroment import IEnvironment
+
+class Production(IEnvironment):
+    def __init__(self):
+        super(Production, self).__init__()
+        externalIp = str([(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1])
+        self.configDict = {
+                                'ip_database':'database',
+                                'port_database':'27017',
+                                'name_database':'kayoo',
+                                'ip_app': externalIp,
+                                'port_app':'8000',
+                                'name_app':'kayoo',
+                                'protocol_app':'http'
+                            }
+
+    def config(self):
+        return self.configDict
