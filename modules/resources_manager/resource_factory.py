1de11d44abe477f593b92fe08e655446a16d89c7
Kev <kev.ms93@gmail.com>
2017-02-20 14:41:05 +0100

    New configuration Pro-Fixed

--- modules/resources_manager/resource_factory.py
+++ modules/resources_manager/resource_factory.py
@@ -1,8 +1,10 @@
 from configs.environments.development import Development
+from configs.environments.production import Production
 
 class ResourceFactory (object):
     def __new__(cls, environmentName):
         resources = {
-                        "development" : Development
+                        "development" : Development,
+                        "production": Production
                     }
         return resources[environmentName]()
